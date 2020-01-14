e3d16165512fbad8e3f2e45dcb1ed4fea16f9968
Franco Canedo <francanedo1@hotmail.com>
2020-01-14 02:03:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,15 +4,15 @@ def nyc_pigeon_organizer(data)
   data.each { |prop, hash| 
     hash.each { |attribute, array| 
       array.each { |name| 
-        if !new_hash[name]
+        if !new_hash.has_key?(name)
           new_hash[name] = {}
         end
         
-        if !new_hash[name][prop]
+        if !new_hash[name].has_key?(prop)
           new_hash[name][prop] = []
         end
         
-        if !new_hash[name][prop][attribute]
+        if !new_hash[name][prop].include?(attribute)
           new_hash[name][prop] << attribute
         end
       }
