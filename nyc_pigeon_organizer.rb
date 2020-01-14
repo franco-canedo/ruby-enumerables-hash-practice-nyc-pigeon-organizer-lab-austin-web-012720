9555ed328e79ba8e15998745d5a2f78f3691569d
Franco Canedo <francanedo1@hotmail.com>
2020-01-14 01:52:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     hash.each { |attribute, array| 
       array.each { |name| 
         if !new_hash[name]
-          new_hash[name]
+          new_hash[name] = {}
         end
         
       }
