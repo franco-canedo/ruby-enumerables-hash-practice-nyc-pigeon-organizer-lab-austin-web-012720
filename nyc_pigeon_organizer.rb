588e3d68140762bc7f82db10a0e378d538dfbd3d
Franco Canedo <francanedo1@hotmail.com>
2020-01-14 01:35:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,5 @@
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-  names = []
   
-  
-  
- data[:color].map { |color| 
-    names = color.map { |name| name.to_sym  }
-  }
-  
-  new_hash = names.uniq
- 
-  i = 0 
-  while i < data[:color].length do
-    if data[:color][i].include?()
-      
-    end
-  end
   
   
   
